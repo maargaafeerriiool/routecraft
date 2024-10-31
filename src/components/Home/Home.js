db40893587a000521169c8e89a43112798d5114e
maargaafeerriiool <margalida.ferriol.torres@prateducacio.com>
2024-10-31 20:01:54 +0100

    HomePage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,43 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.css';
-import FonsDegradat from './FonsDegradat.png'; // Imatge de fons
-import RunnersImage from './PeopleRunning.png'; // Imatge de figures corrent
-import RouteImage from './Route.png'; // Imatge del recorregut
+import FonsDegradat from './FonsDegradat.png'; // Importa la imatge de fons
+import RunnersImage from './PeopleRunning.png'; // Importa la imatge de les figures corrent
 
 function Home() {
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 1000); // 15 segons
+
+    return () => clearTimeout(timer); // Limpiar el temporizador si el componente se desmonta
+  }, []);
+
   return (
-    <div
-      className="home-background"
+    <div className="home-background"
       style={{
         backgroundImage: `linear-gradient(to bottom, rgba(255, 165, 0, 0.7), rgba(255, 0, 0, 0.5)), url(${FonsDegradat})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
         minHeight: "100vh",
         display: "flex",
-        flexDirection: "column",
-        alignItems: "center", // Centra tots els elements horitzontalment
-        justifyContent: "center", // Centra verticalment el contingut
-        position: "relative",
+        alignItems: "center",
+        justifyContent: "center",
+        position: "relative"
       }}
     >
-      {/* Títol centrat */}
-      <div className="form-container" style={{ textAlign: "center", color: "white", position: "relative", zIndex: 2 }}>
-        <h1 className="gradient-text">ROUTECRAFT</h1>
-      </div>
-
-      {/* Imatge de figures corrent a sobre del text */}
-      <img 
-        src={RunnersImage} 
-        alt="Figures corrent" 
-        className="runners-image" 
-      />
-      
-      {/* Distribució de diverses imatges del recorregut */}
-      <div className="routes-container">
-        <img src={RouteImage} alt="Recorregut 1" className="route-image" />
-        <img src={RouteImage} alt="Recorregut 2" className="route-image" />
-        <img src={RouteImage} alt="Recorregut 3" className="route-image" />
-      </div>
+      {showSplash ? (
+        <div className="splash-screen">
+          <h1 className="splash-text">Bienvenido a ROUTECRAFT</h1>
+        </div>
+      ) : (
+        <div className="form-container" style={{ textAlign: "center", color: "white" }}>
+          <h1 className="gradient-text">ROUTECRAFT</h1>
+          <img 
+            src={RunnersImage} 
+            alt="Figures corrent" 
+            className="runners-image" 
+          />
+        </div>
+      )}
     </div>
   );
 }
